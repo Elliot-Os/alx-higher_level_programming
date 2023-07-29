0c01f1f23f9d98cb10f69a50ac8c6741a73dfff6
Elliot-Os <osnathan6@gmail.com>
2023-07-29 06:13:03 -0700

    up

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""
+    0-rectangle: class Rectangle
+"""
 
 
 class Rectangle:
-    """Rectangle class."""
+    """
+        An empty class
+    """
     pass
 
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
